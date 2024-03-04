b30b161d639e9920154749902c7da01762ac2664
IceCreamHand <150932602+IceCreamHand@users.noreply.github.com>
2024-03-04 09:06:16 +0800

    first

---
+++ test1/main.py
@@ -0,0 +1,15 @@
+import sys  #系统文件操作
+#sys.path.append("aaa")
+#from aaa import ttt
+import test
+from PyQt5.QtWidgets import QApplication,QMainWindow
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    MainWindow1 = QMainWindow()  # MainWindow1随便改
+    ui = test.Ui_MainWindow()  # 随便改
+    ui.setupUi(MainWindow1)
+    MainWindow1.show()
+    sys.exit(app.exec_()) #app.exec_()死循环来监听界面关闭按钮等界面控件事件
+
+
---
+++ test1/test2.py
@@ -0,0 +1,22 @@
+import sys
+from PyQt5.QtWidgets import QApplication,QWidget,QPushButton
+from PyQt5.QtGui import QIcon
+
+app = QApplication(sys.argv)
+w = QWidget()#窗体
+w.setWindowTitle("刘金玉编程")
+w.resize(400,300)
+w.setWindowIcon(QIcon("img/img.png"))
+w.setToolTip("编程创造城市")
+
+btn = QPushButton("老刘",w)#按钮控件
+btn.setToolTip("刘金玉编程")
+
+w.show()
+#第二个窗体
+#w2 = QWidget()
+#w2.setWindowIcon(QIcon("img/img.png"))#QWidget设置出来的图标是每个窗体，而QApplication的setwindowQIcon则会设置所有窗体图标(mac系统仅能识别）
+#w2.show()
+
+
+app.exec_()
